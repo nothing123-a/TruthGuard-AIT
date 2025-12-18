7e1a028fb508f17af2a9254fa67e41f8a12f14ef
nothing123-a <kateprajwal0806@gmail.com>
2025-12-19 01:35:13 +0530

    Add .nojekyll and dist folder for GitHub Pages

--- vite.config.js
+++ vite.config.js
@@ -3,9 +3,8 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: './',
+  base: '/TruthGuard-AIT/',
   build: {
-    outDir: 'dist',
-    assetsDir: 'assets'
+    outDir: 'dist'
   }
 })
